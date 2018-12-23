a42d8e0a0ab6c75ceb1e19d28ca224a8bd9c0629
乐陶 <letao@maihaoche.com>
2018-12-23 16:09:10 +0800

    添加测试类

--- lotusroot-web/src/main/java/com/food/lotusroot/web/LotusrootWebApplication.java
+++ lotusroot-web/src/main/java/com/food/lotusroot/web/LotusrootWebApplication.java
@@ -2,9 +2,17 @@ package com.food.lotusroot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LotusrootWebApplication {
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LotusrootWebApplication.class, args);
