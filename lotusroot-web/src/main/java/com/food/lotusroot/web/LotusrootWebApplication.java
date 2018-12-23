cf27ce22ec5e53844640c7a74a6e74eec57f7bfd
乐陶 <letao@maihaoche.com>
2018-12-23 16:04:36 +0800

    项目初建

---
+++ lotusroot-web/src/main/java/com/food/lotusroot/web/LotusrootWebApplication.java
@@ -0,0 +1,14 @@
+package com.food.lotusroot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusrootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotusrootWebApplication.class, args);
+	}
+
+}
+
